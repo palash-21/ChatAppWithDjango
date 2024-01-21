290ab64944dd1ad01db17403de92a19db2e000e0
palash-21 <palashpathak.21@gmail.com>
2024-01-21 17:46:20 +0530

    base html and user added

--- ChatApp/views.py
+++ ChatApp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib.auth import get_user_model
 # Create your views here.
+
+
+User = get_user_model()
+
+
+def index(request):
+    users = User.objects.exclude(username=request.user.username)
+    return render(request, 'index.html', context={'users': users})
+
+
+def chat(request):
+    users = User.objects.exclude(username=request.user.username)
+    return render(request, 'chat.html', context={'users': users})
--- ChatAppWithDjango/urls.py
+++ ChatAppWithDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ChatApp.views import index, chat
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='home'),
+    path('chat/', chat, name='chat'),
 ]
